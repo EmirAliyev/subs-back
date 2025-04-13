d33c85e9baa6ee02fc5f33b5ed81548d077bee91
emiraliyev235 <emiraliyev235@gmail.com>
2025-04-13 19:19:22 +0400

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: isDev()
+      ? '*'
+      : (origin, callback) => {
+        const allowed = ['https://subradar.ru', 'https://www.subradar.ru', 'https://api.subradar.ru'];
+        if (!origin || allowed.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
